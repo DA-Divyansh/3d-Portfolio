4b7c1409219d34bc821dea247713177bd2a7ca25
Divyansh <maddhesiyadivyansh@gmail.com>
2023-12-03 12:29:40 +0530

    Initail commit

---
+++ README.md
@@ -0,0 +1,25 @@
+Project Name: 3D Portfolio Website
+Description
+This project is a 3D portfolio website built using React JS, Three JS, and Tailwind CSS. It serves as a showcase for your skills, projects, and accomplishments in a visually engaging and interactive manner.
+
+Features
+Interactive 3D Environment: Utilizes Three JS to create an immersive and interactive 3D environment for displaying your portfolio items.
+
+Responsive Design: The website is designed to be responsive, ensuring a seamless experience across various devices and screen sizes.
+
+Dynamic Content: Easily update and add new projects, skills, and information through the React JS framework, making the website easy to maintain.
+
+Tailwind CSS Styling: Stylish and modern UI elements are achieved through the use of Tailwind CSS, providing a clean and customizable design.
+
+Email Services: Utilizes emailJS for seamless integration of email services, allowing visitors to contact you directly from the website.
+
+Technologies Used
+React JS: A JavaScript library for building user interfaces.
+Three JS: A cross-browser JavaScript library and application programming interface used to create and display animated 3D computer graphics in a web browser.
+Tailwind CSS: A utility-first CSS framework for rapidly building custom designs.
+EmailJS: A service that simplifies the integration of email functionality in web applications.
+License
+This project is licensed under the MIT License.
+
+Feel free to customize and use it for your own portfolio!
+Thankyou Javascript Mastery (Adrain Hajdin) for your video. 
---
+++ src/constants/index.js
@@ -0,0 +1,232 @@
+import { meta, shopify, starbucks, tesla } from "../assets/images";
+import {
+    car,
+    contact,
+    css,
+    estate,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    linkedin,
+    mongodb,
+    motion,
+    mui,
+    nextjs,
+    nodejs,
+    pricewise,
+    react,
+    redux,
+    sass,
+    snapgram,
+    summiz,
+    tailwindcss,
+    threads,
+    typescript
+} from "../assets/icons";
+
+export const skills = [
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: express,
+        name: "Express",
+        type: "Backend",
+    },
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    },
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: mongodb,
+        name: "MongoDB",
+        type: "Database",
+    },
+    {
+        imageUrl: motion,
+        name: "Motion",
+        type: "Animation",
+    },
+    {
+        imageUrl: mui,
+        name: "Material-UI",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nextjs,
+        name: "Next.js",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nodejs,
+        name: "Node.js",
+        type: "Backend",
+    },
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+    {
+        imageUrl: redux,
+        name: "Redux",
+        type: "State Management",
+    },
+    {
+        imageUrl: sass,
+        name: "Sass",
+        type: "Frontend",
+    },
+    {
+        imageUrl: tailwindcss,
+        name: "Tailwind CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: typescript,
+        name: "TypeScript",
+        type: "Frontend",
+    }
+];
+
+export const experiences = [
+    {
+        title: "React.js Developer",
+        company_name: "Company Name",
+        icon: starbucks,
+        iconBg: "#accbe1",
+        date: "Any Date",
+        points: [
+            "Some Points.",
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "React.js Developer",
+        company_name: "Company Name",
+        icon: tesla,
+        iconBg: "#fbc3bc",
+        date: "Any Date",
+        points: [
+            "Some Points.",
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "Web Developer",
+        company_name: "Company Name",
+        icon: shopify,
+        iconBg: "#b7e4c7",
+        date: "Any Date",
+        points: [
+            "Some Points.",
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "Full stack Developer",
+        company_name: "Company Name",
+        icon: meta,
+        iconBg: "#a2d2ff",
+        date: "Any Date",
+        points: [
+            "Some Points.",
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+];
+
+export const socialLinks = [
+    {
+        name: 'Contact',
+        iconUrl: contact,
+        link: '/contact',
+    },
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/DA-Divyansh',
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: 'https://www.linkedin.com/in/DA-Divyansh',
+    }
+];
+
+export const projects = [
+    {
+        iconUrl: pricewise,
+        theme: 'btn-back-red',
+        name: 'Project Name',
+        description: 'Project Description.',
+        link: 'https://github.com/DA-Divyansh',
+    },
+    {
+        iconUrl: threads,
+        theme: 'btn-back-green',
+        name: 'Project Name',
+        description: 'Project Description.',
+        link: 'https://github.com/DA-Divyansh',
+    },
+    {
+        iconUrl: car,
+        theme: 'btn-back-blue',
+        name: 'Project Name',
+        description: 'Project Description.',
+        link: 'https://github.com/DA-Divyansh',
+    },
+    {
+        iconUrl: snapgram,
+        theme: 'btn-back-pink',
+        name: 'Project Name',
+        description: 'Project Description.',
+        link: 'https://github.com/DA-Divyansh',
+    },
+    {
+        iconUrl: estate,
+        theme: 'btn-back-black',
+        name: 'Project Name',
+        description: 'Project Description.',
+        link: 'https://github.com/DA-Divyansh',
+    },
+    {
+        iconUrl: summiz,
+        theme: 'btn-back-yellow',
+        name: 'Project Name',
+        description: 'Project Description.',
+        link: 'https://github.com/DA-Divyansh',
+    }
+];
